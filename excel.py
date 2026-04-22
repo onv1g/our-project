97ad9f580c13d9544732ef7fabfa1c9140d43790
D1assus1k <logunovi2010@gmail.com>
2026-04-23 00:42:22 +0800

    refactor

--- excel.py
+++ excel.py
@@ -11,23 +11,45 @@ def create_exel_file(percents,data_of_intervals,intervals,filename):
     intervals_beta = []
     orig_gaps = list(data_of_intervals.values())
     orig_percents = list(percents.values())
-    for i in range(1,len(intervals)*2-1):
+    for i in range(1,len(intervals)):
         numbers.append(i)
+
+    n = len(intervals) - 1
+    mid = n // 2
+    q1 = n // 4
+    q3 = (n // 4) * 3
 
     for i in range(1, len(intervals)):
         intervals_angles.append(f"{intervals[i - 1] + 1}-{intervals[i]}")
         intervals_beta.append(f"{(90 - intervals[i - 1]) % 360}-{(90 - intervals[i]) % 360}")
-        gaps_thing.append(orig_gaps[i - 1])
-        gaps_percents.append(orig_percents[i - 1])
+        if q1 < i < mid:
+            idx_mirror = i - 1 + mid
+            if idx_mirror < len(orig_gaps) and orig_gaps[idx_mirror] != 0:
+                gaps_thing.append(orig_gaps[idx_mirror])
+                gaps_percents.append(orig_percents[idx_mirror])
+            else:
+                gaps_thing.append(orig_gaps[i - 1])
+                gaps_percents.append(orig_percents[i - 1])
+
+        elif i < q3:
+            idx_mirror = i - 1 - mid
+            if idx_mirror >= 0 and orig_gaps[idx_mirror] != 0:
+                gaps_thing.append(orig_gaps[idx_mirror])
+                gaps_percents.append(orig_percents[idx_mirror])
+            else:
+                gaps_thing.append(orig_gaps[i - 1])
+                gaps_percents.append(orig_percents[i - 1])
+
+        else:
+
+            gaps_thing.append(orig_gaps[i - 1])
+            gaps_percents.append(orig_percents[i - 1])
 
 
-    for i in range(1, len(intervals)):
 
 
-        intervals_angles.append(f"{(intervals[i - 1] + 180) + 1}-{(intervals[i] + 180)}")
-        intervals_beta.append(f"{(90 - (intervals[i - 1] + 180)) % 360}-{(90 - (intervals[i] + 180)) % 360}")
-        gaps_thing.append(orig_gaps[i - 1])
-        gaps_percents.append(orig_percents[i - 1])
+
+
 
 
     print(numbers)
--- rose.py
+++ rose.py
@@ -90,6 +90,9 @@ def create_rose(input_field, data, window,input_field_filename):
         interval = 10
     else:
         interval = int(interval)
+
+    intervals_azimuths = list(range(0, 361, interval))
+
     intervals_azimuths = []
     intervals_azimuths.append(0)
     d = 0
@@ -98,6 +101,7 @@ def create_rose(input_field, data, window,input_field_filename):
         d = interval * r
         intervals_azimuths.append(d)
         r += 1
+
     data_of_intervals = {}
     for i in range(1, len(intervals_azimuths)):
         quantity_of_angles_in_interval = 0
@@ -128,22 +132,21 @@ def create_rose(input_field, data, window,input_field_filename):
 
 
     print(values)
-    print(angles)
+    print("углы",angles)
 
     mirrored_angles = []
     mirrored_values = []
 
     for i in range(len(angles)):
-
         angle_rad = np.deg2rad(angles[i] - interval / 2)
         val = values[i]
 
-
-        mirrored_angles.append(angle_rad)
+        # Добавляем основной угол (ограничиваем 360 градусами через % 2*pi)
+        mirrored_angles.append(angle_rad % (2 * np.pi))
         mirrored_values.append(val)
 
-
-        mirrored_angles.append(angle_rad + np.pi)
+        # Добавляем зеркальный угол (ограничиваем 360 градусами через % 2*pi)
+        mirrored_angles.append((angle_rad + np.pi) % (2 * np.pi))
         mirrored_values.append(val)
     all_angles = mirrored_angles
     all_values = mirrored_values
